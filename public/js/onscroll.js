e84e580b5d0d20afe6841b16e3a4c7d930cb87f3
SyauqiAlifI <vstudio776@gmail.com>
2022-09-10 11:01:31 +0700

    courses dikit aja

--- public/js/onscroll.js
+++ public/js/onscroll.js
@@ -1,18 +1,10 @@
 // Navbar handler
-window.onscroll = function () {fixnav(); showFooter();}
+window.onscroll = function () {fixnav();}
 function fixnav() {
     let home_nav = document.getElementById('main-nav')
     if (document.documentElement.scrollTop > 1) {
         home_nav.className = "fixed-nav"
     } else {
         home_nav.className = ""
-    }
-}
-function showFooter() {
-    let footer = document.getElementById('our-footer')
-    if (document.documentElement.scrollTop > 1280) {
-        footer.className = " active-foot"
-    } else {
-        footer.className = ""
     }
 }
